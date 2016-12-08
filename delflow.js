27eb8230b8567ee6e18496b8a7e788339eab8a77
Simon Hailes <btsimonh@googlemail.com>
2016-12-08 20:14:20 +0000

    change Addflow to cope with multi-level subflows by expansion.  Still too many diagnostics...

--- delflow.js
+++ delflow.js
@@ -86,7 +86,12 @@ module.exports = function (RED) {
 
             
             try {
-                modifyflow(flow);
+                node.error("call modify");
+                
+                modifyflow(node, flow, msg);
+                node.error("done modify");
+                //msg.payload = flow;
+                node.error("call addFlow");
                 var newflowidpromise = RED2.nodes.addFlow(flow);
 
                 newflowidpromise.then( function(newflowid){
@@ -109,17 +114,313 @@ module.exports = function (RED) {
     RED.nodes.registerType("addflow", addflow);
 
     
+    function clone(a){
+        return JSON.parse(JSON.stringify(a));
+    }
+
+    function newids(node, nodes, ins, outs){
+        var l = nodes.length;
+        // generate new node ids,
+        // and replace any of each old id with the new id 
+        for (var a = 0; a < l; a++){
+            // now for each wire.
+            var orgnodeid = nodes[a].id;
+            nodes[a].id = RED2.util.generateId();
+            node.error("nodeid " + orgnodeid +"->"+nodes[a].id);
+
+            if (ins){
+                // rewire ins and outs of subflow
+                for (var i = 0; i < ins.length; i++){
+                    for (var w = 0; w < ins[i].wires.length; w++){
+                        if (ins[i].wires[w].id === orgnodeid){
+                            node.error("ins " + ins[i].wires[w].id +"->"+nodes[a].id);
+                            ins[i].wires[w].id = nodes[a].id; 
+                        }
+                    }
+                }
+            }
+
+            if (outs){
+                for (var o = 0; o < outs.length; o++){
+                    for (var w = 0; w < outs[o].wires.length; w++){
+                        if (outs[o].wires[w].id === orgnodeid){
+                            node.error("outs " + outs[o].wires[w].id +"->"+nodes[a].id);
+                            outs[o].wires[w].id = nodes[a].id; 
+                        }
+                    }
+                }
+            }
+            
+            for (var b = 0; b < l; b++){
+                if (nodes[b].wires){
+                    if (nodes[b].wires.length){
+                        for (var c = 0; c < nodes[b].wires.length; c++){
+                            var wire = nodes[b].wires[c];
+                            if (wire.length){
+                                for (var d = 0; d < wire.length; d++){
+                                    var w = wire[d];
+                                    if (w === orgnodeid){
+                                        nodes[b].wires[c][d] = nodes[a].id;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                // replace in any keys which mention the old id - 
+                // this will hook up configuration nodes
+                // e.g. serial, mqtt, etc.
+                var keys = Object.keys(nodes[b]);
+                keys.forEach(function(key){
+                   if (nodes[b][key] === orgnodeid){
+                       nodes[b][key] = nodes[a].id;
+                   } 
+                });
+            }
+        }
+    }
+    
+    function flattensubflows(node, f, z, newz){
+        var foundone = false;
+        
+        var nodes = f.nodes;
+        
+        if (!newz) 
+            newz = z;
+        var l = nodes.length;
+        node.error("flatten - newz " + newz);
+        
+        for (var a = 0; a < l; a++){
+            // if node in this flow/subflow
+            if (nodes[a].z === z){
+                if (nodes[a].type.startsWith("subflow:")){
+                    var subflowid = nodes[a].type.slice(8);
+                    node.error("flatten found subflow " + nodes[a].name);
+                    var subflow = null;
+                    var subflowindex = null;
+                    
+                    // find subflow
+                    for (var b = 0; b < l; b++){
+                        if (nodes[b].id === subflowid){
+                            subflow = nodes[b];
+                            subflowindex = b;
+                            node.error("subflow is of " + subflow.name);
+                            break;
+                        }
+                    }
+
+                    // now get all the nodes which belong to this subflow.
+                    // and duplicate them into subnodes
+                    var subnodes = [];
+                    for (var b = 0; b < l; b++){
+                        if (nodes[b].z === subflowid){
+                            // duplicate
+                            //var util = require('util');
+                            var n = nodes[b];
+                            node.error("node:" + util.inspect(n));
+                            var nnode = clone(n);
+                            nnode.z = newz;
+                            subnodes.push(nnode);
+                            node.error("subflow contains " + nnode.name + " type " + nnode.type );
+                            node.error("node:" + util.inspect(nnode));
+                        }
+                    }
+                    
+                    node.error("ins:" + util.inspect(subflow.in, {depth:6}));
+                    var ins = clone(subflow.in);
+                    node.error("ins:" + util.inspect(ins, {depth:6}));
+                    node.error("outs:" + util.inspect(subflow.out, {depth:6}));
+                    var outs = clone(subflow.out);
+                    node.error("outs:" + util.inspect(outs, {depth:6}));
+                    
+                    // now give them unique ids, reconnect internal wires, and connect
+                    // external wires
+                    newids(node, subnodes, ins, outs);
+                    
+                    node.error("ins:" + util.inspect(ins, {depth:6}));
+                    node.error("outs:" + util.inspect(outs, {depth:6}));
+
+                    node.error("new subnodes:" + util.inspect(subnodes));
+
+
+                    // retain the subnodeinstance
+                    var OrgSubflowInstance = nodes[a];
+                    // remove original from list
+                    nodes.splice(a, 1);
+                    // add in our new nodes, else we won't find them in rewire
+                    var newnodes2 = nodes.concat(subnodes);
+                    f.nodes = newnodes2;
+                    nodes = f.nodes;
+                    l = nodes.length;
+
+                    var subflowname = OrgSubflowInstance.name;
+                    if (!subflowname || subflowname === "")
+                        subflowname = subflow.name;
+                    if (!subflowname || subflowname === "")
+                        subflowname = "subflow";
+
+                    // an input and output nodes
+                    var inputnode = 
+                        {
+                            id: OrgSubflowInstance.id, // so inputs are already routed
+                            type: "function",
+                            z: f.id,
+                            name: subflowname + "-in",
+                            func: "\nreturn msg;",
+                            outputs: 1,
+                            noerr: 0,
+                            x: OrgSubflowInstance.x,
+                            y: OrgSubflowInstance.y,
+                            wires: [[]] // empty to start
+                        };
+                    
+                    
+                    var outputnodetemplate = 
+                        {
+                            id: "", // so inputs are already routed
+                            type: "function",
+                            z: f.id,
+                            name: subflowname + "-out",
+                            func: "\nreturn msg;",
+                            outputs: 1, // this is the count out outputs from this template
+                            noerr: 0,
+                            x: OrgSubflowInstance.x + 100,
+                            y: OrgSubflowInstance.y,
+                            wires: [] // empty to start
+                        };
+                    
+                    var outputnodes = [];
+                    // creat as many as we have 'outs', regardless of connection
+                    for (var out = 0; out < outs.length; out++){
+                        var output = clone(outputnodetemplate);
+                        output.id = RED2.util.generateId();
+                        output.name = output.name + (out+1);
+                        // only try to connect if any.
+                        if (OrgSubflowInstance.wires[out]){
+                            output.wires.push(OrgSubflowInstance.wires[out]);
+                        }
+                        output.y = output.y + out * 40;
+                        outputnodes.push(output);
+                    }
+
+                    
+                    // add input and output nodes; so we can find then in the next bit
+                    // only need input if it has any output
+                    nodes.push(inputnode);
+
+                    // if outputs are not connected, they will not exist...
+                    newnodes2 = nodes.concat(outputnodes);
+                    f.nodes = newnodes2;
+                    nodes = f.nodes;
+                    l = nodes.length;
+                                        
+                    // we will have a new id
+                    var newsubflowid = subflow.id;
+                    
+                    // wire up input and outputs
+                    
+                    // output must get sent from all mentioned in outs
+                    for (var out1 = 0; out1 < outs.length; out1 ++){
+                        for (var outw = 0; outw < outs[out1].wires.length; outw++){
+                            // the dest
+                            var dest = outputnodes[out1].id;
+                            var src = outs[out1].wires[outw].id;
+                            var srcport = outs[out1].wires[outw].port;
+                            
+                            node.error("outs "+out1+" wire "+outw+" dest "+dest+" src "+ src+":"+srcport);
+                            
+                            // if out connected to in, then do so.
+                            if (src === newsubflowid){
+                                node.error("is subflow itself");
+                                src = inputnode.id;
+                                srcport = 0;
+                            }
+                            
+                            // find src, and add our node as a dest
+                            for (var srcnode = 0; srcnode < l; srcnode ++){
+                                if (nodes[srcnode].id === src){
+                                    while (!nodes[srcnode].wires[srcport]){
+                                        nodes[srcnode].wires.push([]);
+                                    }
+                                    node.error("found src at node "+srcnode);
+                                    nodes[srcnode].wires[srcport].push(dest);
+                                }
+                            }
+                        }
+                    }
+
+                    // input must send to all mentioned in ins
+                    for (var inp = 0; inp < ins.length; inp ++){
+                        for (var inpw = 0; inpw < ins[inp].wires.length; inpw++){
+                            inputnode.wires[0].push(ins[inp].wires[inpw].id);
+                        }
+                    }
+
+                    // if new input node has no output, then kill it
+                    // by setting z bad, it will be filtered out.
+                    // BUT it may be unconnected on inside, so best leave it here
+                    //if (inputnode.wires.length == 0){
+                    //    inputnode.z = 'xxx';
+                    //}
+                    
+                    // zap the one we found, so we don't find it again
+                    
+                    node.error("flatten kill subflow");
+                    //nodes[a].z = "xxx";
+                    //nodes[a].type = "xxx";
+                    
+                    foundone = true;
+                    node.error("done subflow");
+                    
+                    // must start again, as we've updated the nodes.
+                    // will call back in from the start
+                    break;
+                }
+            }
+        }
+        
+        if (!foundone){
+            node.error("no more subflows found");
+        }
+        return foundone;
+    }
+
     // parses a flow, and makes a new id 
     // for each id we come accross, correctly 
     // re-linking wiring
-    function modifyflow( f ){
+    function modifyflow( node, f, msg ){
+        node.error("in modify");
         if (!f.id){
-            if (f.nodes[0].z){
-                f.id = f.nodes[0].z;
+            //find the first z which is NOT an id
+            var l = f.nodes.length;
+            for (var a = 0; a < l; a++){
+                //if non-empty z
+                node.error("check " + f.nodes[a].z + " " + f.nodes[a].name);
+                if (f.nodes[a].z && (f.nodes[a].z !== "")){
+                    var found = false;
+                    var z = f.nodes[a].z;
+                    for (var b = 0; b < l; b++){
+                        if (z === f.nodes[b].id){
+                            node.error("found at " + f.nodes[b].id + " " + f.nodes[b].name);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found){
+                        node.error("z not an id");
+                        f.id = z;
+                        node.error("z not an id");
+                        node.error("found flow id at node " + a + " " + f.nodes[a].name);
+                        break;
+                    }
+                }
             }
         }
         
         if (f.id){
+            node.error("modify");
+
             var flowid = f.id;
             f.id = RED2.util.generateId();
             
@@ -127,34 +428,50 @@ module.exports = function (RED) {
             var l = nodes.length;
             for (var a = 0; a < l; a++){
                 // first, modify owner of node.
-                if (nodes[a].z == flowid){
+                if (nodes[a].z === flowid){
+                    nodes[a].z = f.id;
+                }
+                
+                // config nodes normally have z = "", give them us
+                // instead.
+                // this means that they will be deleted when the flow is deleted.
+                if (nodes[a].z === ""){
                     nodes[a].z = f.id;
                 }
             }
-    
-            for (var a = 0; a < l; a++){
-                // now for each wire.
-                var orgnodeid = nodes[a].id;
-                nodes[a].id = RED2.util.generateId();
-                for (var b = 0; b < l; b++){
-                    if (nodes[b].wires){
-                        if (nodes[b].wires.length){
-                            for (var c = 0; c < nodes[b].wires.length; c++){
-                                var wire = nodes[b].wires[c];
-                                if (wire.length){
-                                    for (var d = 0; d < wire.length; d++){
-                                        var w = wire[d];
-                                        if (w === orgnodeid){
-                                            nodes[b].wires[c][d] = nodes[a].id;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+
+            // for each subflow, flatten it.
+            // call until we find no more; each iteration 
+            // flattens one level
+            node.error("call flatten");
+            var done = false;
+            do {
+                done = flattensubflows(node, f, f.id);
+            } while (done);
+
+            // f.nodes is changed in flatten...
+            
+            // generate new node ids,
+            // and replace any of each old id with the new id 
+            newids(node, f.nodes);
+            
+            // finally discard anything not marked with our z
+            var ournodes = f.nodes.filter(function(onode){ 
+                if((onode.z === f.id) && (!onode.type.startsWith("subflow:")) ){
+                    return true;
                 }
-            }
+                });
+
+            msg.payload = JSON.stringify(f);
+                
+            f.nodes = ournodes;
+            
+            msg.payload = msg.payload + JSON.stringify(f);
+            
+        } else {
+            node.error("modify - no id found?");
         }
+
     }
     
 };
